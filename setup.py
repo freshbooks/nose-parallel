a7b378dd915e8e90c2c381cfa4706d9bb6e6524b
mattKaczorowski <matthewkaczorowski@gmail.com>
2017-06-20 00:46:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='nose-parallel',
-    version='0.2.1',
+    version='1706.2.1',
     description=u'A nosetests plugin to split test suites to run in parallel',
     long_description=open('README.rst').read(),
     author=', '.join([
